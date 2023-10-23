1bacd335ee36a182f6c0845b0435a28db5eded7d
Vonney <davonnejv@gmail.com>
2023-10-23 09:59:08 -0500

    adding react project for landing page

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import TopHeader from "./components/TopHeader";
+
+function App() {
+  return <TopHeader />;
+}
+
+export default App;
---
+++ src/components/TopHeader.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import Hotels from "./Hotels";
+import Events from "./Events";
+import Book from "./Book";
+import Explore from "./Explore";
+
+export default function TopHeader() {
+  return (
+    <>
+      <Link to="/hotels">Hotels</Link>
+      <Link to="/events">Events</Link>
+      <Link to="/explore">Explore</Link>
+      <Link to="/book">Book </Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<Hotels />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/book" element={<Book />} />
+      </Routes>
+    </>
+  );
+}
