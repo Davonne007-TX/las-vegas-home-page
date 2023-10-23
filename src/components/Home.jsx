6c233eff613a2f6d5e1ef61d2ee22c4ee04cda38
Vonney <davonnejv@gmail.com>
2023-10-23 12:18:42 -0500

    simplifying code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 import "./css/Home.css";
-import { Routes, Route, Link } from "react-router-dom";
-import Hotels from "./Hotels";
-import Events from "./Events";
-import Book from "./Book";
-import Explore from "./Explore";
+import MyLinks from "./MyLinks";
 
 export default function Home() {
   return (
     <div className="home-container">
-      <Link to="/">Las Vegas</Link>
-      <br />
-      <Link to="/hotels">Hotels</Link>
-      <Link to="/events">Events</Link>
-      <Link to="/explore">Explore</Link>
-      <Link to="/book">Book </Link>
+      <MyLinks />
     </div>
   );
 }
