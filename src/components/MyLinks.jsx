29a757b31088005ba585e96e4f664c8aad6b1859
Vonney <davonnejv@gmail.com>
2023-10-23 13:23:28 -0500

    adding font and styles to myLinks

--- src/components/MyLinks.jsx
+++ src/components/MyLinks.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./css/MyLinks.css";
 
 export default function MyLinks() {
   return (
     <>
-      <Link to="/">Las Vegas</Link>
-      <br />
-      <Link to="/hotels">Hotels</Link>
-      <Link to="/events">Events</Link>
-      <Link to="/explore">Explore</Link>
-      <Link to="/book">Book </Link>
+      <div className="myLinks">
+        <Link to="/">Welcome To Las Vegas</Link>
+        <br />
+        <Link to="/hotels">Hotels</Link>
+        <Link to="/events">Events</Link>
+        <Link to="/explore">Explore</Link>
+        <Link to="/book">Book </Link>
+      </div>
     </>
   );
 }
