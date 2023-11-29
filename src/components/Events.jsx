c8cbf60c5634e0ee820432375d61612015e41ccb
Vonney <davonnejv@gmail.com>
2023-11-28 19:43:28 -0600

    adding background image to Events.jsx

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import MyLinks from "./MyLinks";
+import "./css/Events.css";
 
 export default function Events() {
   return (
     <>
       <MyLinks />
-      <div>Events</div>
+      <div className="dealer">
+        <h1>Blackjack</h1>
+      </div>
     </>
   );
 }
